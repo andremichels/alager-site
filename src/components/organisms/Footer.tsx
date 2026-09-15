794f92f62725891fccf14b41d2f29c4ec8e8a24d
Andre Michels <andre.michels@gmail.com>
2026-09-15 14:35:32 -0300

    fix: enlarge logo (header 40px, footer 48px) for legibility

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -44,7 +44,7 @@ export function Footer({ locale }: FooterProps) {
           {/* Col 1 */}
           <div>
             <a href={`/${locale}`} style={{ display: "flex", alignItems: "center", textDecoration: "none", marginBottom: 24 }}>
-              <Logo height={32} variant="white" />
+              <Logo height={48} variant="white" />
             </a>
             <p style={linkStyle}>{t("tagline")}</p>
             <div style={{ marginTop: 24 }}>
