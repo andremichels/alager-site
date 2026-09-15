2c3654b5d76d8e08eebe9ebabe085c9fef075ac1
Andre Michels <andre.michels@gmail.com>
2026-09-15 14:51:56 -0300

    chore: pin Sanity studio appId for external deploy

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -6,4 +6,7 @@ export default defineCliConfig({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "your-project-id",
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   },
+  deployment: {
+    appId: "bsb08wsyd9y6emklrgetyna5",
+  },
 });
